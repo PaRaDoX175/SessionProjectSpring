159880055b75c7a5f5d13ee3c24874b53c174b78
Ruslan Absalyamov <ruslan.absalam@gmail.com>
2026-05-10 23:14:42 +0500

    Add vacancy response controller

--- src/main/java/org/example/newsessionproject/controllers/AbsalyamovRuslanVacancyResponseController.java
+++ src/main/java/org/example/newsessionproject/controllers/AbsalyamovRuslanVacancyResponseController.java
@@ -1,10 +1,50 @@
 package org.example.newsessionproject.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanAddVacancyResponseDto;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanChangeProposalStatusDto;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanUserDetails;
+import org.example.newsessionproject.entities.AbsalyamovRuslanVacancyResponse;
+import org.example.newsessionproject.services.AbsalyamovRuslanVacancyResponseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/vacancyResponse")
 public class AbsalyamovRuslanVacancyResponseController {
+    private final AbsalyamovRuslanVacancyResponseService vacancyResponseService;
 
+    public AbsalyamovRuslanVacancyResponseController(AbsalyamovRuslanVacancyResponseService vacancyResponseService) {
+        this.vacancyResponseService = vacancyResponseService;
+    }
+
+    @GetMapping("/vacancy/{vacancyId}")
+    @PreAuthorize("hasRole('CLIENT')")
+    public List<AbsalyamovRuslanVacancyResponse> getResponsesForClient(@PathVariable Long vacancyId) {
+        return vacancyResponseService.getResponsesForClient(vacancyId);
+    }
+
+    @GetMapping("/freelancer")
+    @PreAuthorize("hasRole('FREELANCER')")
+    public List<AbsalyamovRuslanVacancyResponse> getResponsesForFreelancer(@AuthenticationPrincipal AbsalyamovRuslanUserDetails user) {
+        return vacancyResponseService.getResponsesForFreelancer(user.getId());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('FREELANCER')")
+    public void addResponse(@RequestBody @Valid AbsalyamovRuslanAddVacancyResponseDto dto) {
+        vacancyResponseService.addResponse(dto);
+    }
+
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('CLIENT')")
+    public AbsalyamovRuslanVacancyResponse changeProposalStatus(@PathVariable Long id,
+                                                                @RequestBody @Valid AbsalyamovRuslanChangeProposalStatusDto dto) {
+        return vacancyResponseService.changeProposalStatus(id, dto);
+    }
 }
