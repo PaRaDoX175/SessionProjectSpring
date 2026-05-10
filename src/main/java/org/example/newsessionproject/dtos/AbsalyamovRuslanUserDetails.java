f5c171a7f3e39e26960bdbb24ec937d4cbcae145
Ruslan Absalyamov <ruslan.absalam@gmail.com>
2026-05-10 19:03:58 +0500

    Add custom user details and jwt filter

---
+++ src/main/java/org/example/newsessionproject/dtos/AbsalyamovRuslanUserDetails.java
@@ -0,0 +1,42 @@
+package org.example.newsessionproject.dtos;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AbsalyamovRuslanUserDetails implements UserDetails {
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public AbsalyamovRuslanUserDetails(Long id, String email, String password,
+                             Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
--- src/main/java/org/example/newsessionproject/middlewares/AbsalyamovRuslanJwtAuthFilter.java
+++ src/main/java/org/example/newsessionproject/middlewares/AbsalyamovRuslanJwtAuthFilter.java
@@ -1,7 +1,57 @@
 package org.example.newsessionproject.middlewares;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanUserDetails;
+import org.example.newsessionproject.services.AbsalyamovRuslanJwtService;
+import org.example.newsessionproject.services.AbsalyamovRuslanUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Service
-public class AbsalyamovRuslanJwtAuthFilter {
+public class AbsalyamovRuslanJwtAuthFilter extends OncePerRequestFilter {
+    private final AbsalyamovRuslanUserDetailsService userDetailsService;
+    private final AbsalyamovRuslanJwtService jwtService;
+
+    public AbsalyamovRuslanJwtAuthFilter(AbsalyamovRuslanUserDetailsService userDetailsService,
+                                         AbsalyamovRuslanJwtService jwtService) {
+        this.userDetailsService = userDetailsService;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var auth = request.getHeader("Authorization");
+
+        if (auth == null || !auth.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        var token = auth.substring(7);
+        var email = jwtService.getEmailFromToken(token);
+        var id = jwtService.getIdFromToken(token);
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = userDetailsService.loadUserByUsername(email);
+
+            AbsalyamovRuslanUserDetails cud = new AbsalyamovRuslanUserDetails(id,
+                    userDetails.getUsername(),
+                    userDetails.getPassword(),
+                    userDetails.getAuthorities());
+
+            if (jwtService.isEmailCorrect(email, userDetails)) {
+                var authToken = new UsernamePasswordAuthenticationToken(cud, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
