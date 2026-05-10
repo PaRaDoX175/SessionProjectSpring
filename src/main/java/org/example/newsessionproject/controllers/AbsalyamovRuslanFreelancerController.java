74bba49a3fa91caa3c79f5d0597a638f5c03cc8a
Ruslan Absalyamov <ruslan.absalam@gmail.com>
2026-05-10 20:42:58 +0500

    Add freelancer controller

--- src/main/java/org/example/newsessionproject/controllers/AbsalyamovRuslanFreelancerController.java
+++ src/main/java/org/example/newsessionproject/controllers/AbsalyamovRuslanFreelancerController.java
@@ -1,6 +1,10 @@
 package org.example.newsessionproject.controllers;
 
 import jakarta.validation.Valid;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanAddResumeDto;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanFreelancerResponseDto;
+import org.example.newsessionproject.dtos.AbsalyamovRuslanUserDetails;
+import org.example.newsessionproject.services.AbsalyamovRuslanFreelancerService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -8,5 +12,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/freelancer")
 public class AbsalyamovRuslanFreelancerController {
+    private final AbsalyamovRuslanFreelancerService freelancerService;
 
+    public AbsalyamovRuslanFreelancerController(AbsalyamovRuslanFreelancerService freelancerService) {
+        this.freelancerService = freelancerService;
+    }
+
+    @GetMapping
+    public AbsalyamovRuslanFreelancerResponseDto getFreelancer(@AuthenticationPrincipal AbsalyamovRuslanUserDetails user) {
+        return freelancerService.getFreelancer(user.getId());
+    }
+
+    @PatchMapping
+    @PreAuthorize("hasRole('FREELANCER')")
+    public AbsalyamovRuslanFreelancerResponseDto addResume(@RequestBody @Valid AbsalyamovRuslanAddResumeDto dto,
+                                           @AuthenticationPrincipal AbsalyamovRuslanUserDetails user) {
+        return freelancerService.addResume(user.getId(), dto);
+    }
 }
