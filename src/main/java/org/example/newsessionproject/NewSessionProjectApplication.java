902128f9c4488e48e99e0c3f5198769c44cb71c7
Ruslan Absalyamov <ruslan.absalam@gmail.com>
2026-05-10 18:01:06 +0500

    Initial commit

---
+++ src/main/java/org/example/newsessionproject/NewSessionProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.newsessionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSessionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSessionProjectApplication.class, args);
+    }
+
+}
